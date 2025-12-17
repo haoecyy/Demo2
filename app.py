2c1682c273a654d87d95d633de344c4a0f510efd
haoecyy <haoecyy0127@gmail.com>
2025-12-17 12:06:10 +0800

    Add Streamlit title and introductory text

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.title("Welcome to Streamlit")
+st.write("Hello guys")
+st.write("my name is arina")
+st.write("**Bold text**")
+         
