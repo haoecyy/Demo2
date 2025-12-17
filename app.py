287823262122567b5233e5b3fa761f17df1b3306
haoecyy <haoecyy0127@gmail.com>
2025-12-17 12:10:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.title("Welcome to Streamlit")
 st.write("Hello guys and **Bold Text**")
-st.write("Hello guys",**Bold Text**)
+
 st.write("my name is arina")
 st.write({"Name":"Alice","Age":3})
 st.write("**Bold text**")
